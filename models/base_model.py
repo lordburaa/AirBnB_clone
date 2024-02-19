bfcbea359b9fb3b6801f90141f323462d5b2afd0
lordburaa <comrdebiruk97@gmail.com>
2024-02-19 11:27:22 -0800

    airbnb

--- models/base_model.py
+++ models/base_model.py
@@ -1,51 +1,34 @@
 #!/usr/bin/python3
 """
-Contains class BaseModel
+Base mode 
+
 """
+import date
 
-from datetime import datetime
-import models
-import uuid
-
-time = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel:
-    """The BaseModel class from which future classes will be derived"""
-
-    def __init__(self, *args, **kwargs):
-        """Initialization of the base model"""
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
-            if hasattr(self, "created_at") and type(self.created_at) is str:
-                self.created_at = datetime.strptime(kwargs["created_at"], time)
-            if hasattr(self, "updated_at") and type(self.updated_at) is str:
-                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            models.storage.new(self)
-            models.storage.save()
+    """base Model class creaed"""
+    def __init__(self, id=None):
+        self.id = str(uuid.uuid4())
+        self.created_at = date
+        self.updated_at = chage 
 
     def __str__(self):
-        """String representation of the BaseModel class"""
-        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
-                                         self.__dict__)
+        return f"{self.__class__.__name__} ({self.id}) {self.__dict__}"
+
+    def created_at(self, *args):
+        pass
+
+    @classmethod
+    def updated_at(cls):
+        pass
 
     def save(self):
-        """updates the attribute 'updated_at' with the current datetime"""
-        self.updated_at = datetime.now()
-        models.storage.save()
+        self.updated_at =
+
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of the instance"""
-        new_dict = self.__dict__.copy()
-        if "created_at" in new_dict:
-            new_dict["created_at"] = new_dict["created_at"].strftime(time)
-        if "updated_at" in new_dict:
-            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
-        new_dict["__class__"] = self.__class__.__name__
-        return new_dict
+
+        return self.__dict__
+
