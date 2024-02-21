a45994b27f5f5a3d5e533e86b48e177e609ee1ea
lordburaa <comrdebiruk97@gmail.com>
2024-02-21 03:18:57 -0800

    update the time

--- models/base_model.py
+++ models/base_model.py
@@ -13,7 +13,7 @@ class BaseModel:
         """inistantiatio"""
         self.created_at = datetime.now()
         self.id = str(uuid.uuid4())
-        self.updated_at = self.created_at
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """str representation"""
