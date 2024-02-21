7774b24c3d18ecad09b3dc41ebadaf2c79628bbd
lordburaa <comrdebiruk97@gmail.com>
2024-02-21 04:37:03 -0800

    fix the isoformat for attribute

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,7 @@ class BaseModel:
     """base Model class creaed"""
     def __init__(self, *args, **kwargs):
         """inistantiatio"""
-        self.created_at = datetime.now().isoformat()
+        self.created_at = datetime.now()
         self.id = str(uuid.uuid4())
         self.updated_at = self.created_at
 
@@ -26,7 +26,7 @@ class BaseModel:
         """to dictionary"""
         new_dict = self.__dict__.copy()
         new_dict["created_at"] = self.created_at
-        new_dict["updated_at"] = self.updated_at
         new_dict["__class__"] = self.__class__.__name__
+        new_dict["updated_at"] = self.updated_at
         sort_t = dict(sorted(new_dict.items(), reverse=True))
         return sort_t
