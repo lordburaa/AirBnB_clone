e20b5ca1befe6b7f3c4330baeac0ba7ccd40441a
lordburaa <comrdebiruk97@gmail.com>
2024-02-20 22:08:55 -0800

    adding the  base class

--- models/base_model.py
+++ models/base_model.py
@@ -1,34 +1,29 @@
 #!/usr/bin/python3
 """
-Base mode 
+Base mode
 
 """
-import date
-
+from datetime import datetime
+import uuid
 
 
 class BaseModel:
     """base Model class creaed"""
-    def __init__(self, id=None):
+    def __init__(self, *args, **kwargs):
+        """inistantiatio"""
         self.id = str(uuid.uuid4())
-        self.created_at = date
-        self.updated_at = chage 
+        self.created_at = str(datetime.now().isoformat('-'))
+        self.updated_at = str(datetime.now().isoformat('-'))
 
     def __str__(self):
-        return f"{self.__class__.__name__} ({self.id}) {self.__dict__}"
-
-    def created_at(self, *args):
-        pass
-
-    @classmethod
-    def updated_at(cls):
-        pass
+        """str representation"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        self.updated_at =
-
+        """save the time"""
+        self.updated = str(datetime.now().isoformat('-'))
 
     def to_dict(self):
-
+        """to dictionary"""
+        self.__dict__["__class__"] = "BaseModel"
         return self.__dict__
-
