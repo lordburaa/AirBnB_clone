b542f94683ce5297a280ee05890a85dbc3922df0
lordburaa <comrdebiruk97@gmail.com>
2024-02-21 06:02:49 -0800

    fix the error

--- models/base_model.py
+++ models/base_model.py
@@ -17,13 +17,13 @@ import uuid
 
 class BaseModel:
     """base Model class creaed"""
-    created_at = datetime.now()
-    updated_at = datetime.now()
     
     def __init__(self):
         """instianation"""
         self.id = str(uuid.uuid4())
-    
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+         
     def __str__(self):
         """str representation"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
