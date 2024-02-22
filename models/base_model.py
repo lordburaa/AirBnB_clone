6c56eed032bd2b5ec0ceb529593f5ba3ed473caa
lordburaa <comrdebiruk97@gmail.com>
2024-02-21 22:46:33 -0800

    changing the conditon to self_dict

--- models/base_model.py
+++ models/base_model.py
@@ -20,11 +20,11 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """instianation"""
         if kwargs:
-            if "id" in kwargs:
+            if "id" not in self.__dict__:
                 self.id = str(uuid.uuid4())
-            if "created_at" in kwargs:
+            if "created_at" not in self.__dict__:
                 setattr(self, "created_at", datetime.now())
-            if "updated_at" in kwargs:
+            if "updated_at" not in self.__dict__:
                 self.updated_at = datetime.now()
 
 
