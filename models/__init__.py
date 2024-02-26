0a9f4fd4c2e146312124cec3719096c55ed2f037
lordburaa <comrdebiruk97@gmail.com>
2024-02-25 21:59:55 -0800

    add and remove

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-"""
-initialize the models package
-"""
 
-from models.engine.file_storage import FileStorage
-
+from .engine.file_storage import FileStorage
 storage = FileStorage()
+
 storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python3
 """
-Contains class BaseModel
-"""
+Module for the Base Class
+-public instnace attributes
+    -id
+    -created_at
+    -updated_at
 
+-Public instnace methods
+    -save(self) - updates the updated_at attribute
+    -to_dict(self) - returns dictionary
+
+"""
 from datetime import datetime
-import models
 import uuid
-<<<<<<< HEAD
 from models import storage
 from .engine.file_storage import FileStorage
-=======
-
-time = "%Y-%m-%dT%H:%M:%S.%f"
-
->>>>>>> 7ce3f01b92beec18e2fb52cb2e19b78a2a12cf36
 
 class BaseModel:
-    """The BaseModel class from which future classes will be derived"""
-
+    """base Model class creaed"""
     def __init__(self, *args, **kwargs):
-        """Initialization of the base model"""
+        """instianation"""
         if kwargs:
-<<<<<<< HEAD
             self.__dict__ = kwargs.copy()
             if "id" not in self.__dict__:
                 setattr(self, "id", str(uuid.uuid4()))
@@ -30,38 +29,26 @@ class BaseModel:
             if "updated_at" not in self.__dict__:
                 setattr(self, "updated_at", datetime.now())
             storage.new(self)
-=======
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
-            if hasattr(self, "created_at") and type(self.created_at) is str:
-                self.created_at = datetime.strptime(kwargs["created_at"], time)
-            if hasattr(self, "updated_at") and type(self.updated_at) is str:
-                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
->>>>>>> 7ce3f01b92beec18e2fb52cb2e19b78a2a12cf36
         else:
+
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            models.storage.new(self)
-            models.storage.save()
-
+            storage.new(self)
+         
     def __str__(self):
-        """String representation of the BaseModel class"""
-        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
-                                         self.__dict__)
+        """str representation"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """updates the attribute 'updated_at' with the current datetime"""
+        """save the time"""
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of the instance"""
+        """to dictionary"""
         new_dict = self.__dict__.copy()
-        if "created_at" in new_dict:
-            new_dict["created_at"] = new_dict["created_at"].strftime(time)
-        if "updated_at" in new_dict:
-            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
         new_dict["__class__"] = self.__class__.__name__
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
         return new_dict
