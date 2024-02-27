fd6c2c08c719cbb4b0711f2a7b639ecd551fbd0e
lordburaa <comrdebiruk97@gmail.com>
2024-02-27 00:20:54 -0800

    modify the id

--- models/base_model.py
+++ models/base_model.py
@@ -26,8 +26,6 @@ class BaseModel:
         
         if kwargs:
             self.__dict__ = kwargs.copy()
-            if "id" not in self.__dict__:
-                setattr(self, "id", str(uuid.uuid4()))
             if "created_at" not in self.__dict__:
                 setattr(self, "created_at", datetime.now())
             if "updated_at" not in self.__dict__:
