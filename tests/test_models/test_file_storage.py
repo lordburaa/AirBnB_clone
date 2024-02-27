67cdb25d7b9b4a8fec7ce99196ebb515e7a2d247
lordburaa <comrdebiruk97@gmail.com>
2024-02-26 22:59:10 -0800

    adding the test for the filestorage

---
+++ tests/test_models/test_file_storage.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+unnittest
+"""
+import models
+class TestFileStorage(unittest.TestCase):
+    """Test File Storag """
+
+    @classmethod
+    def setUpClass():
+        my_model = BaseModel()
+        my_model.name = "My_First_Model"
+        my_model.my_number = 89
+        my_model.save()
+    
+    def test_all(self):
+        """Testing the all method """
+        assert isinstance(type(my_dict), dict)
+
+if __name__ == '__main__':
+    unittest.main()
