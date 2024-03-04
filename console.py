8b7b501c90155e6fa55d1bd7ba58acfce41e9b81
lordburaa <comrdebiruk97@gmail.com>
2024-03-04 00:06:02 -0800

    checking wheter it added succesfuly

--- console.py
+++ console.py
@@ -3,11 +3,101 @@
     console
 """
 import cmd
-
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     """cmd module"""
     prompt = "(hbnb) "
+    
+    def do_create(self, line):
+        """command to create new instnace of BaseMode save to the FILE and print id"""
+        if line == '':
+            print("** class name missing **")
+        elif line != "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            obj = BaseModel()
+            obj.save()
+            print(obj.id)
+    
+    def do_show(self, line):
+        show_id = storage.all()
+        list_t = line.split()
+        if len(list_t) < 1:
+            print("** class name is missing **")
+        elif list_t[0] not in "BaseModel":
+            print("** class doesn't exist **")
+        elif len(list_t) < 2:
+            print("** instance id missing **")
+        else:
+            check_dic ="BaseModel."+ str(list_t[1])
+            print(check_dic)
+            if check_dic not in show_id:
+                print("** no instance found **")
+            else:
+                print(show_id)
+
+
+    def do_destroy(self, line):
+        """destroy command"""
+
+        del_id = storage.all()
+        list_t = line.split()
+        
+        if len(list_t) < 1:
+            print("** class name is missing **")
+        elif list_t[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif len(list_t) < 2:
+            print("** instnace id missing **")
+        else:
+            if list_t[1] not in del_id:
+                print("** no instance found **")
+            else:
+                #del
+                del_id.clear()
+                #save the ins
+                del_id.save()
+
+    def do_all(self, line):
+        """Print all string representaiton of all instnaces based or not on the class name"""
+        all_dict = storage.all()
+        list_t = line.split()
+        
+        if len(list_t) >= 1:
+            if list_t[0] != "BaseModel":
+                print("** class doesn't exist **")
+            else:
+                print(BaseModel())
+        else:
+            print(all_dict)
+
+    def do_update(self, line):
+        """Update an instance based on the class name and id by adding or updating attribute"""
+        list_t = line.split()
+        dic_obj = storage.all()
+        check_id = "BaseModel."+ str(list_t[1])
+        if len(list_t) < 1:
+            print("** class name missing **")
+        elif list_t[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif len(list_t) < 2:
+            print("** instance id missing **")
+        elif check_id not in dic_obj:
+            #checking wheter id instnace is exist
+            print("** no instance found **")
+
+        elif len(list_t) < 3:
+            print("** attribute name is missing **")
+        elif len(list_t) < 4:
+            print("** value missing **")
+        else:
+            print("ALL ARGUMET IS NOT USED")
+
+
+
 
     def do_quit(self, line):
         """Quit command to exit the program\n"""
@@ -15,7 +105,7 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         """empty command to loop empty interactive shell"""
-        return False
+        pass
 
     def do_EOF(self, line):
         """Exis the interactive shell"""
