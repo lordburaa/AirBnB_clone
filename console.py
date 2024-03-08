1567a99d5ad16ca94c1be8dee44ac4996bc7575d
lordburaa <comrdebiruk97@gmail.com>
2024-03-08 05:28:03 -0800

    fix import

--- console.py
+++ console.py
@@ -8,7 +8,11 @@ import cmd
 import os
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import State
 from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from models.engine.file_storage import FileStorage
 from models import storage
 
