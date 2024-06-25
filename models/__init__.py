f59cb226cf2653c49829b0ba0d2f6010ce25b903
lordburaa <comrdebiruk97@gmail.com>
2024-06-25 01:43:10 -0700

    fix hte base class

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ python module intialization """
 from models.engine.file_storage  import FileStorage
-from models.base_model import Basemodel
 storage = FileStorage()
 
 storage.reload()
