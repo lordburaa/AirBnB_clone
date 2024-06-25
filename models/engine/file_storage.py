58472b20c6ac53a2fb67138b0e5af8b1100e726f
lordburaa <comrdebiruk97@gmail.com>
2024-06-24 23:30:22 -0700

    fix the file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,18 +20,22 @@ class FileStorage:
     def new(self, obj):
         """ returning new func"""
         key = "{}.{}".format(type(obj), obj.id)
-        self.__objects[key] = obj.__str__()
+        self.__objects[key] = obj
 
     def save(self):
         """ save file json format"""
         r = {}
         tmp = {}
+        do = {}
         if os.path.exists(self.__file_path):
             with open("file.json") as t:
                 r = json.load(t)
-        tmp = {**r, **self.__objects}
+        for key, value in self.__objects.items():
+            tmp[key] = value.__str__()
+        do = {**tmp, **r}
+
         with open(self.__file_path, 'w') as f:
-            json.dump(tmp, f)
+            json.dump(do, f)
 
     def reload(self):
         """ reload json file """
---
+++ re
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm file.json
+./test_save_reload_base_model.py 
