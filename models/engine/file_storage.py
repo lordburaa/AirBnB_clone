21836451e5389d82f0ef0d82c5457a937545a0e4
lordburaa <comrdebiruk97@gmail.com>
2024-06-25 00:23:57 -0700

    fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,7 +2,7 @@
 """ file storage class """
 import json
 import os
-
+import datetime
 
 
 class FileStorage:
@@ -20,21 +20,17 @@ class FileStorage:
 
     def new(self, obj):
         """ returning new func"""
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj
+        if obj:
+
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            self.__objects[key] = obj
 
     def save(self):
         """ save file json format"""
         r = {}
-        tmp = {}
-        if os.path.exists(self.__file_path):
-            with open(self.__file_path, 'r', encoding="utf-8") as t:
-                r = json.load(t)
         for key, value in self.__objects.items():
-            tmp[key] = value.__str__()
-        r = {**r, **tmp}
-
-        with open(self.__file_path, 'w') as f:
+            r[key] = value.to_dict()
+        with open(self.__file_path, 'w', encoding="UTF-8") as f:
             json.dump(r, f)
 
     def reload(self):
