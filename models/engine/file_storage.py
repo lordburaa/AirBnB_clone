76584bdab7c46228e3aafd86393f68c270ebdb00
lordburaa <comrdebiruk97@gmail.com>
2024-06-24 21:35:30 -0700

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,14 +4,13 @@ import json
 import os
 
 
+
 class FileStorage:
     """ storage for the file """
 
     __file_path = "file.json"
     __objects = {}
 
-    def __init__(self):
-        pass
 
     def all(self):
         """ returing all method """
@@ -20,21 +19,22 @@ class FileStorage:
     def new(self, obj):
         """ returning new func"""
         key = f"{obj.__class__.__name__}.{obj.id}"
-        self.__objects[key] = obj.__str__()
+        self.__objects[str(key)] = obj.__str__()
 
     def save(self):
         """ save file json format"""
         r = {}
         tmp = {}
         if os.path.exists(self.__file_path):
-            with open('file.json') as t:
+            with open("file.json") as t:
                 r = json.load(t)
         tmp = {**r, **self.__objects}
+        print(tmp)
         with open(self.__file_path, 'w') as f:
             json.dump(tmp, f)
 
     def reload(self):
         """ reload json file """
         if (os.path.exists(self.__file_path)):
-            with open('file.json') as f:
+            with open("file.json") as f:
                 self.__objects = json.load(f)
