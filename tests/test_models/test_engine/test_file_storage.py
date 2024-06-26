ffd329b3dd68a807771b1b80aa6a3b09e4035550
lordburaa <comrdebiruk97@gmail.com>
2024-06-26 01:10:51 -0700

    fix pycodestyle

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -5,12 +5,11 @@ import os
 import json
 from models.base_model import BaseModel
 import unittest
-from  models.engine.file_storage import FileStorage
-
+from models.engine.file_storage import FileStorage
 
 
 class TestFileStorage(unittest.TestCase):
     """ unittest for the file storage """
-    
+
     def test_file_storage_instance(self):
-        self.assertEqual(FileStorage, type(FileStorage)))
+        self.assertEqual(FileStorage, type(FileStorage))
