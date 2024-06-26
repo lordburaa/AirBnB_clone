f2ffdad264811a49ea1f9064bb2af6f3b6917af4
lordburaa <comrdebiruk97@gmail.com>
2024-06-26 01:56:13 -0700

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -28,10 +28,6 @@ class BaseModel:
                     pass
                 else:
                     setattr(self, key, value)
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
         else:
             storage.new(self)
 
