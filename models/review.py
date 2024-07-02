1c676cafcbe73b0c6213bafa61200601b31e494c
lordburaa <comrdebiruk97@gmail.com>
2024-07-02 02:46:05 -0700

    checking pycodestyle

--- models/review.py
+++ models/review.py
@@ -4,9 +4,9 @@ from models.base_model import BaseModel
 from models.place import Place
 from models.user import User
 
+
 class Review(BaseModel):
     """Review Model """
     palace_id = ''
     user_id = ''
     text = ''
-
